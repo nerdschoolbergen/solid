3b95bcb85aac5b152e59fdc357883b8fe4eedf2a
Eirik Årdal <eaardal@outlook.com>
2016-03-01 10:25:21 +0100

    Implemented Task 1

--- exercise/alarmsystem/src/main/java/nerdschool/FireSensor.java
+++ exercise/alarmsystem/src/main/java/nerdschool/FireSensor.java
@@ -1,19 +1,30 @@
 package nerdschool;
 
 public class FireSensor implements Sensor {
+    private static final int triggeredPercentage = 10;
+    private int nrOfIsTriggeredCalls = 0;
+    private int batteryPercentage = 100;
+
     public boolean isTriggered() {
+        batteryPercentage -= 10;
+
+        nrOfIsTriggeredCalls++;
+        if (nrOfIsTriggeredCalls % triggeredPercentage == 0) {
+            nrOfIsTriggeredCalls = 0;
+            return true;
+        }
         return false;
     }
 
     public String getLocation() {
-        return null;
+        return "Lobby 1st floor";
     }
 
     public String getSensorType() {
-        return null;
+        return "Fire";
     }
 
     public double getBatteryPercentage() {
-        return -1;
+        return batteryPercentage;
     }
 }
--- exercise/alarmsystem/src/test/java/nerdschool/FireSensorTest.java
+++ exercise/alarmsystem/src/test/java/nerdschool/FireSensorTest.java
@@ -4,27 +4,54 @@ import junit.framework.TestCase;
 
 public class FireSensorTest extends TestCase {
 
-    public void testThatIsTriggeredReturnsFalse() {
+    public void testThatIsTriggeredReturnsTrueAbout10PercentOfTheTime() {
         FireSensor sensor = new FireSensor();
-        boolean isTriggered = sensor.isTriggered();
-        assertEquals(false, isTriggered);
+
+        final float nrOfPolls = 100;
+        float triggeredCount = 0;
+
+        for (int i = 0; i < nrOfPolls; i++) {
+            if (sensor.isTriggered()) {
+                triggeredCount++;
+            }
+        }
+
+        final int allowedOffset = 2;
+        final int expectedPercentage = 10;
+
+        float triggeredPercentage =  triggeredCount / nrOfPolls * 100;
+        int maxAllowedOffset = expectedPercentage + allowedOffset;
+        int minAllowedOffset = expectedPercentage - allowedOffset;
+
+        assertTrue(triggeredPercentage >= minAllowedOffset && triggeredPercentage <= maxAllowedOffset );
     }
 
     public void testThatGetLocationReturnsNull(){
         FireSensor sensor = new FireSensor();
         String location = sensor.getLocation();
-        assertEquals(null, location);
+        assertEquals("Lobby 1st floor", location);
     }
 
     public void testThatGetSensorTypeReturnsNull(){
         FireSensor sensor = new FireSensor();
         String sensorType = sensor.getSensorType();
-        assertEquals(null, sensorType);
+        assertEquals("Fire", sensorType);
     }
 
-    public void testThatGetBatteryPercentageReturnsNegativeOne(){
+    public void testThatGetBatteryPercentageDrains10PercentBetweenEachPoll(){
         FireSensor sensor = new FireSensor();
-        double batteryPercentage = sensor.getBatteryPercentage();
-        assertEquals(-1.0, batteryPercentage);
+
+        double batteryPercentage1 = sensor.getBatteryPercentage();
+        sensor.isTriggered();
+
+        double batteryPercentage2 = sensor.getBatteryPercentage();
+        sensor.isTriggered();
+
+        double batteryPercentage3 = sensor.getBatteryPercentage();
+        sensor.isTriggered();
+
+        assertEquals(100.0, batteryPercentage1);
+        assertEquals(90.0, batteryPercentage2);
+        assertEquals(80.0, batteryPercentage3);
     }
 }
